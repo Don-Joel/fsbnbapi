775689c01bfeb8df26abd232cc0e06c70c5bff41
DonJoel <jt3bk@virginia.edu>
2020-11-15 17:34:45 -0500

    configuring heroku db

--- migrations/20190719080020-provider.js
+++ migrations/20190719080020-provider.js
@@ -1,36 +1,36 @@
-// 'use strict';
+'use strict';
 
-// var dbm;
-// var type;
-// var seed;
+var dbm;
+var type;
+var seed;
 
-// /**
-//   * We receive the dbmigrate dependency from dbmigrate initially.
-//   * This enables us to not have to rely on NODE_PATH.
-//   */
-// exports.setup = function(options, seedLink) {
-//   dbm = options.dbmigrate;
-//   type = dbm.dataType;
-//   seed = seedLink;
-// };
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
 
-// exports.up = function (db, callback) {
-//   db.createTable('provider',
-//   {
-//     id: { type: 'int', length: 6, primaryKey: true, autoIncrement: true },
-//     name: { type: 'string', length: 50},
-//     email: { type: "string", length: 50},
-//     address: { type: "string", length: 50},
-//     dateCreated: { type: "string", length: 50 }
-//   },
-//    callback());
-// }
+exports.up = function (db, callback) {
+  db.createTable('provider',
+  {
+    id: { type: 'int', length: 6, primaryKey: true, autoIncrement: true },
+    name: { type: 'string', length: 50},
+    email: { type: "string", length: 50},
+    address: { type: "string", length: 50},
+    dateCreated: { type: "string", length: 50 }
+  },
+   callback());
+}
 
-// exports.down = function(db, callback) {
-//   db.dropTable('provider');
-//   callback();
-// };
+exports.down = function(db, callback) {
+  db.dropTable('provider');
+  callback();
+};
 
-// exports._meta = {
-//   "version": 3
-// };
+exports._meta = {
+  "version": 3
+};
--- src/database/database.js
+++ src/database/database.js
@@ -2,23 +2,24 @@
 
 const mysql = require("mysql");
 
-const config = {
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "fs_bnb"
-};
-
+//local db
 // const config = {
-//   host: "us-cdbr-iron-east-02.cleardb.net",
+//   host: "localhost",
 //   port: 3306,
-//   user: "bc05126ff0e150",
-//   password: "6910055d",
-//   database: "heroku_a3ee1eece26bd24"
+//   user: "root",
+//   password: "root",
+//   database: "fs_bnb"
 // };
 
-//bc05126ff0e150:6910055d@us-cdbr-iron-east-02.cleardb.net/heroku_a3ee1eece26bd24?reconnect=true
+//heroku db 
+const config = {
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  port: 3306,
+  user: "bc05126ff0e150",
+  password: "6910055d",
+  database: "heroku_a3ee1eece26bd24"
+};
+
 mysql: var connection = mysql.createConnection(config);
 
 connection.connect(err => {
